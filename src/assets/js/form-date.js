02acf9381cc8d1cbc680a8b1d2ae7703f5a002ce
Payton Chertude <p.chertude34@gmail.com>
2025-02-05 18:50:12 -0800

    calendar form

---
+++ src/assets/js/form-date.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  const addDateButton = document.getElementById('add-date-button');
+  addDateButton.addEventListener('click', addDatePickerToForm);
+});
+
+
+function addDatePickerToForm(event) {
+
+  event.preventDefault();
+  console.log('date p8icker adding');
+  const formDateContainer = document.getElementById('date-input-container');
+  const existingDatePickerCount = formDateContainer.children.length;
+
+  if (existingDatePickerCount >= 3) return;
+
+  const dateInputGroup = document.createElement('div');
+  dateInputGroup.classList.add('cs-date-input-group');
+
+  // create the date label and input
+  const dateLabel = document.createElement('label');
+  dateLabel.classList.add('cs-label');
+
+  const dateInput = document.createElement('input');
+  dateInput.classList.add('cs-input');
+  dateInput.type = 'date';
+  // Set the minimum date to today
+  dateInput.min = new Date().toISOString().split('T')[0];
+
+  dateLabel.appendChild(dateInput);
+
+  // create the time select label and input
+  const timeLabel = document.createElement('label');
+  timeLabel.classList.add('cs-label');
+
+  const timeInput = document.createElement('select');
+  timeInput.classList.add('cs-select');
+
+  const morningOption = document.createElement('option');
+  morningOption.value = 'morning';
+  morningOption.textContent = 'Morning';
+  timeInput.appendChild(morningOption);
+
+  const afternoonOption = document.createElement('option');
+  afternoonOption.value = 'afternoon';
+  afternoonOption.textContent = 'Afternoon';
+  timeInput.appendChild(afternoonOption);
+
+  timeLabel.appendChild(timeInput);
+
+  // put it all together
+  dateInputGroup.appendChild(dateLabel);
+  dateInputGroup.appendChild(timeLabel);
+
+  formDateContainer.appendChild(dateInputGroup);
+
+  if (formDateContainer.children.length >= 3) {
+    addDateButton.disabled = true;
+  }
+}
+
